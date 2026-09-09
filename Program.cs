79ddcf0782da9750c0045744a1b58136b504ccf4
a26thoir <a26thoir@student.his.se>
2026-09-09 12:49:12 +0200

    Add files via upload
    Added new functionality to project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace TextAdventure_Solo_2;
+﻿﻿namespace TextAdventure_Solo_2;
 
 class Program
 {
@@ -12,30 +12,33 @@ class Program
 //Adding extra empty lines with the writeline directly below. Purely for aesthetics.
         Console.WriteLine("\nTell me... ");
 
-string action1 = "check";
-string action2 = "pick up";
-string action3 = "use";
-string action4 = "hit";
 
-string playername = "";
-do
-{
+        string action1 = "check";
+        string action2 = "pick up";
+        string action3 = "use"; 
+        string action4 = "hit";
 
-    Console.Write("What is your name? ");
 
-    playername = Console.ReadLine();
+        string playername = "";
+        do
+        {
+            playername = Ask("What is your name, delinquent? ");
+        } while (!AskYesOrNo($"So, {playername} it is? [yes/ ok] / [no] "));
 
-    Console.Write($"So, {playername} it is? ");
-    string yesOrNo = Console.ReadLine().Trim().ToLower();
-    
-    if (yesOrNo != "yes" && yesOrNo != "ok")
-    {
-        playername = "";
-    }
+        Console.WriteLine("");
+        Console.WriteLine($"{playername}, you are surrounded by four walls of the most awful beige hue. " +
+                          $"A flickering ceiling-lamp is casting a depressing glow upon you surroundings. " +
+                          $"You have no items to your name, and are dressed in naught but a grey one-piece, " +
+                          $"spotted by what you assume to be bodily fluids. You have no sense of direction. " +
+                          $"One solitary door marked by countless scratches is crammed into a corner of the room. " +
+                          $"In the middle of the room, directly under the lamp, is a square wooden table with" +
+                          $" a box on top.");
 
-} while (playername == "");
+//Design decision to display the player's available actions in the writeline for efficiency.
+        Console.WriteLine("");
+        Console.Write($"What do you do? [{action1}], [{action2}], [{action3}], [{action4}] :");
 
-Console.WriteLine("");
+        Console.ReadLine();Console.WriteLine("");
 Console.WriteLine($"{playername}, you are surrounded by four walls of the most awful beige hue. " +
                   $"A flickering ceiling-lamp is casting a depressing glow upon you surroundings. " +
                   $"You have no items to your name, and are dressed in naught but a grey one-piece, " +
@@ -50,16 +53,41 @@ Console.Write($"What do you do? [{action1}], [{action2}], [{action3}], [{action4
 
 Console.ReadLine();
 
-// static string Ask(string question)
-// {
-// string response;
-// do
-// {
-// Console.Write(question);
-// response = Console.ReadLine().Trim();
-// } while (response == "");
 
-// return response;
-// }
+    }
+    
+    static string Ask(string question)
+    {
+        string response;
+        do
+        {
+            Console.Write(question);
+            response = Console.ReadLine().Trim();
+        } while (response == "");
+
+        return response;
+    }
+
+    static bool AskYesOrNo(string question)
+    {
+        while (true)
+        {
+            string response = Ask(question).ToLower();
+            switch (response) {
+                case "yes":
+                case "ok":
+                    return true;
+                case "no":
+                    return false;
+            }
+        }
+    }
+
+    class Hero
+    {
+        public string Name = "";
+        public int Sanity = 100;
+        public List<string> Items = new List<string>();
+        public string Location = "newgame";
     }
 }
